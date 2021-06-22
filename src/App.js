473554ffad1a69aa7efb563705bfbe01864778ed
Astig Mandalian <astigmandalian@Astigs-MacBook-Air.local>
2021-06-22 19:39:28 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Darts Master Practice II
         </a>
       </header>
     </div>
