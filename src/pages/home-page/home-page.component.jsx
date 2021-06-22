62b7436ee01fc96e3852facb922da605b9577593
Astig Mandalian <astigmandalian@Astigs-MacBook-Air.local>
2021-06-22 20:28:49 +0100

    Installed react-router-dom and created the home page component

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './home-page.styles.css';
+
+const HomePage = () => {
+    return(
+        <div className='home-page'>
+            <h1>DARTS MASTER PRACTICE II</h1>
+        </div>
+    );
+};
+
+export default HomePage;
