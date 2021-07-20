f23e37edc23669b01b6fddf27c137ca195090a9d
Astig Mandalian <astigmandalian@Astigs-MacBook-Air.local>
2021-07-20 20:49:58 +0100

    Styled select-name

--- src/components/select-name/select-name.component.jsx
+++ src/components/select-name/select-name.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './select-name.styles.css';
+
 import { setName } from '../../redux/name/name-action';
 
 const SelectName = ({ setName }) => {
     return(
         <input
+            className='select-name'
             placeholder='Enter your name'
             onChange={e => {{setName(e.target.value)}}}
         />
