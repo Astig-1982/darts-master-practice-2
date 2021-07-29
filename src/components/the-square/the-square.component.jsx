f8e27d6493ffcd7da6c789c1d59846a9933d9a9b
Astig Mandalian <astigmandalian@Astigs-MacBook-Air.local>
2021-07-29 21:24:32 +0100

    Added if statement to check if the value of the square is NaN

--- src/components/the-square/the-square.component.jsx
+++ src/components/the-square/the-square.component.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { setSquareResults } from '../../redux/stats/stats.actions';
+import { isTheValueNaN, setSquareResults } from '../../redux/stats/stats.actions';
 
 import './the-square.styles.css';
 
-const TheSquare = ({setSquareResults, theClass}) => {
-    console.log(theClass)
+const TheSquare = ({setSquareResults, valueIsNaN, theClass}) => {
     return(
         <input 
             className={`the-square ${theClass}`}
-            onBlur={() => {{setSquareResults(theClass)}}}
+            onBlur={(e) => {isNaN(parseInt(e.target.value)) ? valueIsNaN() :  setSquareResults(theClass)}}
         />
     );
 };
@@ -19,7 +18,8 @@ const mapDispatchToProps = dispatch => ({
     // this action is being pulled from stats.actions and it takes the class of each square
     // the class is taken to stats.reducer, which will call addToResults() function with the class as as argument
     // addToResult() will take the class and based on it, it will get the element's value
-    setSquareResults: theClass => dispatch(setSquareResults(theClass)) 
+    setSquareResults: theClass => dispatch(setSquareResults(theClass)),
+    valueIsNaN: () => dispatch(isTheValueNaN()) 
 });
 
 export default connect(
